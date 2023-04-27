b30613ec931c1b705607fd3c10173c2fd2c161f0
DimaWork <dditskiy@gmail.com>
2023-04-27 17:15:14 +0300

    users finish

--- src/App.js
+++ src/App.js
@@ -2,13 +2,57 @@ import React from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
+import { useState } from "react"
+import { useEffect } from 'react';
+
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [invaise, setInvaise] = useState([])
+  const [isLoading, setLoading] = useState(true)
+  const [success, setSuccess] = useState(false)
+  const [searchValue, setSearchValue] = useState("")
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then(res => res.json())
+      .then(json => setUsers(json.data))
+      .catch(err => console.log(err))
+      .finally(() => setLoading(false))
+  }, []);
+
+  const onCangeSearchValue = (e) => {
+    setSearchValue(e.target.value)
+  }
+
+  const onClicInvait = (id) => {
+    if (invaise.includes(id)) {
+      setInvaise(prev => prev.filter(_id => _id !== id))
+    } else {
+      setInvaise( prev => [...prev, id])
+    }
+  }
+
+  const onSabmitClick = () => {
+    setSuccess(!success)
+  }
+
   return (
     <div className="App">
-      <Users />
+      {
+        success ? <Success 
+        onSabmitClick={onSabmitClick} 
+        invaise={invaise}/> : <Users 
+        onSabmitClick={onSabmitClick}
+        invaise={invaise}
+        onClicInvait={onClicInvait}
+        onCangeSearchValue={onCangeSearchValue}
+        searchValue={searchValue} 
+        items={users} 
+        isLoading={isLoading}/>
+      }
       {/* <Success /> */}
     </div>
   );
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export const Success = ({ count }) => {
+export const Success = ({ onSabmitClick , invaise}) => {
+  const count = invaise.length
   return (
     <div class="success-block">
       <img src="/assets/success.svg" alt="Success" />
       <h3>Успешно!</h3>
       <p>Всем {count} пользователям отправлено приглашение.</p>
-      <button className="send-invite-btn">Назад</button>
+      <button onClick={onSabmitClick}  className="send-invite-btn">Назад</button>
     </div>
   );
 };
